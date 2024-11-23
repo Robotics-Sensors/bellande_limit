348c966470c5c25d4791d7170c2837e712349c42
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-11-22 21:01:31 -0500

    latest pushes

--- Package/python/setup.py
+++ Package/python/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("../../README.md", "r", encoding="utf-8") as fh:
+with open("./README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="bellande_limit",
-    version="0.1.1",
+    version="0.1.2",
     description="Robots Limit",
     long_description=long_description,
     long_description_content_type="text/markdown",
